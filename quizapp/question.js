f42923d1f2f3de6019450598f254f2fa65ff4330
Comfort <65386761+cnorteye@users.noreply.github.com>
2020-07-20 17:31:54 -0700

    adding javascript projects

---
+++ quizapp/question.js
@@ -0,0 +1,96 @@
+// Questions from: https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+
+var questions = [
+
+	{
+		"question": "Which of the following is correct about JavaScript?",
+		"option1": "JavaScript is a lightweight, interpreted programming language.",
+		"option2": "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages.",
+		"option3": " The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers.",
+		"option4": "All of the above.",
+		"answer": "4"
+
+	}, 
+
+	{
+		"question": "Which of the following is correct about features of JavaScript?",
+		"option1": "JavaScript is a lightweight, interpreted programming language.",
+		"option2": "JavaScript is designed for creating network-centric applications.",
+		"option3": "JavaScript is complementary to and integrated with Java.",
+		"option4": "All of the above.",
+		"answer": "4"
+	}, 
+
+	{
+		"question": "Which of the following is correct about features of JavaScript?",
+		"option1": "JavaScript is is complementary to and integrated with HTML.",
+		"option2": "JavaScript is open and cross-platform.",
+		"option3": "Both of the above.",
+		"option4": "All of the above.",
+		"answer": "3"
+	}, 
+
+	{
+		"question": "Which of the following is an advantage of using JavaScript?",
+		"option1": "Less server interaction.",
+		"option2": " Immediate feedback to the visitors.",
+		"option3": " Increased interactivity.",
+		"option4": "All of the above.",
+		"answer": "4"
+	}, 
+
+	{
+		"question": "Which of the following is a disadvantage of using JavaScript?",
+		"option1": "Client-side JavaScript does not allow the reading or writing of files.",
+		"option2": "JavaScript can not be used for Networking applications because there is no such support available.",
+		"option3": "JavaScript doesn't have any multithreading or multiprocess capabilities.",
+		"option4": "All of the above.",
+		"answer": "4"
+	}, 
+
+	{
+		"question": "Which of the following is the correct syntax to redirect a url using JavaScript?",
+		"option1": "document.location='http://www.newlocation.com';",
+		"option2": "browser.location='http://www.newlocation.com';",
+		"option3": "navigator.location='http://www.newlocation.com';",
+		"option4": "window.location='http://www.newlocation.com';",
+		"answer": "4"
+	},
+
+	 {
+		"question": "Which of the following is true about variable naming conventions in JavaScript?",
+		"option1": "JavaScript variable names must begin with a letter or the underscore character.",
+		"option2": "JavaScript variable names are case sensitive.",
+		"option3": "Both of the above.",
+		"option4": "All of the above.",
+		"answer": "3"
+	}, 
+
+	{
+		"question": "Which of the following is true about typeof operator in JavaScript?",
+		"option1": "The typeof is a unary operator that is placed before its single operand, which can be of any type.",
+		"option2": "Its value is a string indicating the data type of the operand.",
+		"option3": "Both of the above.",
+		"option4": "All of the above.",
+		"answer": "3"
+	}, 
+
+	{
+		"question": "Which of the following is true about cookie handling in JavaScript?",
+		"option1": "JavaScript can manipulate cookies using the cookie property of the Document object.",
+		"option2": "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+		"option3": "Both of the above.",
+		"option4": "All of the above.",
+		"answer": "3"
+	}, 
+
+	{
+		"question": "Which of the following is the correct syntax to create a cookie using JavaScript?",
+		"option1": "document.cookie = 'key1 = value1; key2 = value2; expires = date';",
+		"option2": "browser.cookie = 'key1 = value1; key2 = value2; expires = date';",
+		"option3": "window.cookie = 'key1 = value1; key2 = value2; expires = date';",
+		"option4": " navigator.cookie = 'key1 = value1; key2 = value2; expires = date';",
+		"answer": "0"
+	}
+
+];
